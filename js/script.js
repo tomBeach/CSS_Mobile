4ba1854cbccf3ad9de866b1e6556806413fdd723
tomBeach <tbeach2k@gmail.com>
2017-09-01 06:46:15 -0400

    updatd scroll script for mobile

--- js/script.js
+++ js/script.js
@@ -4,109 +4,113 @@ $(document).ready(function() {
 
     function initialize() {
         console.log("== initialize ==");
-        $('.menu-btn[0]').on('mousedown', toggleMenuLink);
-        $('.menu-btn').on('mousedown', toggleMenuLink);
-        // $('.menu-btn').on('mouseup', toggleMenuLink);
         $('#toggle-menu-btn').on('mousedown', toggleMenu);
+        console.log("window.innerWidth:", window.innerWidth);
+        console.log("window.innerHeight:", window.innerHeight);
+        if (window.innerWidth < 321) {
+            checkImageWH("D_pic");
+        }
     }
     function toggleMenu() {
         console.log("== toggleMenu ==");
         if ($('#side-menu').css('margin-left') == '-120px') {
             $('#side-menu').css('margin-left', '0');
+            // == activate benu buttons
+            // $('.menu-btn[0]').on('mousedown', toggleMenuLink);
+            $('.menu-btn').on('mousedown', toggleMenuLink);
+            // $('.menu-btn').on('mouseup', toggleMenuLink);
         } else {
             $('#side-menu').css('margin-left', '-120px');
         }
     }
     function toggleMenuLink(e) {
         console.log("== toggleMenuLink ==");
-        console.log("$('.menu-btn')[0]:", $('.menu-btn')[0]);
-        console.log("$('.menu-btn').length:", $('.menu-btn').length);
-        console.dir( $._data(day, 'events') );
-        console.dir( $._data(week, 'events') );
-        var whichPix = e.currentTarget.id;
-        var dayScroll = $('#day-pix').offset().top - 60;
-        var weekScroll = $('#week-pix').offset().top - 60;
-        var monthScroll = $('#month-pix').offset().top - 60;
-        console.log("whichPix:", whichPix);
-        console.log("dayScroll:", dayScroll);
-        console.log("weekScroll:", weekScroll);
-        console.log("monthScroll:", monthScroll);
-        if ($('#' + whichPix).css('background-color') == 'rgb(14, 11, 22)') {
+        // == change menu-item bg color
+        if ($('#' + e.currentTarget.id).css('background-color') == 'rgb(14, 11, 22)') {
             $('.menu-btn').css('background-color', 'rgb(14, 11, 22)');
-            $('#' + whichPix).css('background-color', 'rgb(255, 0, 0)');
+            $('#' + e.currentTarget.id).css('background-color', 'rgb(255, 0, 0)');
         } else {
-            $('#' + whichPix).css('background-color', 'rgb(14, 11, 22)');
+            $('#' + e.currentTarget.id).css('background-color', 'rgb(14, 11, 22)');
         }
-        switch (whichPix) {
+
+        // == scroll window to selected section
+        switch (e.currentTarget.id) {
             case "day":
-            var dayScroll = $('#day-pix').offset().top - 60;
-            var weekScroll = $('#week-pix').offset().top - 60;
                 $('html, body').animate({
-                    scrollTop: dayScroll
+                    scrollTop: $('#D_header').offset().top - 60
                 }, 300);
                 break;
             case "week":
-            var dayScroll = $('#day-pix').offset().top - 60;
-            var weekScroll = $('#week-pix').offset().top - 60;
                 $('html, body').animate({
-                    scrollTop: weekScroll
+                    scrollTop: $('#W_header').offset().top - 60
                 }, 300);
                 break;
             case "month":
                 $('html, body').animate({
-                    scrollTop: monthScroll
+                    scrollTop: $('#M_header').offset().top - 60
+                }, 300);
+                break;
+            case "year":
+                $('html, body').animate({
+                    scrollTop: $('#Y_header').offset().top - 60
                 }, 300);
                 break;
             default:
-
+                $('html, body').animate({
+                    scrollTop: $('#D_header').offset().top - 60
+                }, 300);
+                break;
         }
     }
     function checkImageWH(imageId) {
         console.log("== checkImageWH ==");
-        // // Get image
-        // var dayImage = $("#" + imageId);
-        // console.log("dayImage:", dayImage);
-        //
-        // // Create new image to test
-        // var newImage = new Image();
-        // newImage.src = dayImage.attr("src");
-        //
-        // // Get accurate measurements from that.
-        // var windowW = $(window).width();
-        // var imageW = newImage.width;
-        // var imageH = newImage.height;
-        // var maxWH = windowW*0.8;
-        // // var sectionH = getSectionHeight(windowW);
-        // var imgT = parseInt($(dayImage).css('top').slice(0, -2));
-        // if (imageW > imageH) {
-        //     console.log("+++ landscape +++");
-        //     var newH = parseInt((maxWH*imageH)/imageW);
-        //     var topOffset = parseInt((320 - newH)/2);
-        //     console.log("topOffset:", topOffset);
-        //     var newT = imgT;
-        //     $(dayImage).css('width', maxWH + 'px');
-        //     $(dayImage).css('height', 'auto');
-        //     $(dayImage).css('top', newT + 'px');
-        //     $(dayImage).css('left', parseInt((320 - maxWH)/2) + 'px');
-        // } else {
-        //     console.log("+++ portrait +++");
-        //     var newW = parseInt((maxWH*imageW)/imageH);
-        //     $(dayImage).css('width', newW + 'px');
-        //     $(dayImage).css('height', maxWH + 'px');
-        //     $(dayImage).css('top', newT + 'px');
-        //     $(dayImage).css('left', parseInt((320 - newW)/2) + 'px');
-        // }
-    }
-    function getSectionHeight(windowW) {
-        console.log("== getSectionHeight ==");
-        if(window.innerHeight > window.innerWidth){
-            console.log("+++ portrait +++");
-        } else {
+        // == Get image
+        var dayImage = $("#" + imageId);
+        var dayBox = $("#D_picBox");
+        console.log("dayImage:", dayImage);
+
+        // == Create new image to test
+        var newImage = new Image();
+        newImage.src = dayImage.attr("src");
+
+        // == Get accurate measurements from that.
+        var windowW = $(window).width();
+        var imageW = newImage.width;
+        var imageH = newImage.height;
+        var maxWH = windowW*0.8;
+        // var sectionH = getSectionHeight(windowW);
+        console.log("$(dayBox).css('top'):", $(dayBox).css('top'));
+
+        // == get top px (remove "px" from top value string)
+        var imgT = parseInt($(dayBox).css('top').slice(0, -2));
+        if (imageW > imageH) {
             console.log("+++ landscape +++");
+            var newH = parseInt((maxWH*imageH)/imageW);
+            var topOffset = parseInt((320 - newH)/2);
+            console.log("topOffset:", topOffset);
+            var newT = imgT;
+            $(dayBox).css('width', maxWH + 'px');
+            $(dayBox).css('height', 'auto');
+            $(dayBox).css('top', newT + 'px');
+            $(dayBox).css('left', parseInt((320 - maxWH)/2) + 'px');
+        } else {
+            console.log("+++ portrait +++");
+            var newW = parseInt((maxWH*imageW)/imageH);
+            $(dayBox).css('width', newW + 'px');
+            $(dayBox).css('height', maxWH + 'px');
+            $(dayBox).css('top', newT + 'px');
+            $(dayBox).css('left', parseInt((320 - newW)/2) + 'px');
         }
-        return windowH;
     }
+    // function getSectionHeight(windowW) {
+    //     console.log("== getSectionHeight ==");
+    //     if(window.innerHeight > window.innerWidth){
+    //         console.log("+++ portrait +++");
+    //     } else {
+    //         console.log("+++ landscape +++");
+    //     }
+    //     return windowH;
+    // }
 
     initialize();
-    checkImageWH("today-pic");
 });
